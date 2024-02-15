4ece4a1f399e0782871c3dfc990ca1b710ed21dc
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2024-02-15 09:35:47 -0500

    clase 4
    se creo una pantalla dos para pasar datos de pantalla 1 a la 2

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
-   Button btn1,btn2;
+   Button btn1,btn2,btnsend;
    String Tag = "Prueba";
 
     @Override
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         nombreUsuario = (TextView) findViewById(R.id.usuario);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
+        btnsend = findViewById(R.id.btnsend);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -39,42 +40,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pasarInformacion();                
+            }
+        });
 
+
+    }
+
+    private void pasarInformacion(){
+        String textoactual = nombreUsuario.getText().toString();
+        String nombre = "Juan Carlos"
+        Intent pasarinfo = new Intent(this,Pantallados.class);
+        pasarinfo.putExtra("name",nombre);
+        startActivity(pasarinfo);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(Tag,"Estoy en OnStart");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(Tag,"Estoy en OnResume");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(Tag,"Estoy en OnPause");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(Tag,"Estoy en OnStop");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.i(Tag,"Estoy en OnRestart");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnStart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(Tag,"Estoy en OnDestroy");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en OnStart");
     }
 }
---
+++ app/src/main/java/co/nelson/appuno/Pantallados.java
@@ -0,0 +1,63 @@
+package co.nelson.appuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Pantallados extends AppCompatActivity {
+
+    TextView usuariodos,textoAnterior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Pantallados);
+        usuariodos = findViewById(R.id.usuariodos);
+        textoAnterior = findViewById(R.id.textoAnterior);
+
+        String name = getIntent().getStringExtra("name");
+        usuariodos.setText(name);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Estoy en OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag,"Estoy en OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
+    }
+}
